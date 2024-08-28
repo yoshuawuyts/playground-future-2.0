b08898c916d7278b98ff14351eaee7137ad7da0c
Yosh <github@yosh.is>
2024-08-28 04:06:51 +0200

    we have a poller!

--- src/main.rs
+++ src/main.rs
@@ -1,196 +1,155 @@
-use rustix::{
-    event::kqueue::{self, Event, EventFilter, EventFlags},
-    io::Errno,
-};
-use std::{
-    io,
-    os::fd::{AsRawFd, OwnedFd, RawFd},
-};
+//! shout out noah kennedy:
+//! https://gist.github.com/yoshuawuyts/c74b0b344f62133664f36d8192367b97
 
-use std::net::TcpListener;
+#![cfg(target_os = "macos")]
 
-#[cfg(target_os = "macos")]
-fn main() -> io::Result<()> {
-    let mut poller = Poller::new().unwrap();
-
-    // Create a new blocking Tcp listener on a un-allocated port:
-    let addr = "127.0.0.1:3456";
-    let listener = AsyncTcpListener::bind(addr)?;
-
-    poller.register(Waitable::Fd(listener.as_raw_fd(), Interest::ReadWrite))?;
-
-    // connect to the listener from another thread to trigger an event:
-    // std::thread::scope(|s| {
-    //     s.spawn(|| {
-    //         let _ = TcpStream::connect(addr).unwrap();
-    //     });
-    // });
-
-    // check for events!
-    let events = poller.wait()?;
-    assert_eq!(events.len(), 1);
-    Ok(())
-}
-
-#[cfg(not(target_os = "macos"))]
-fn main() {
-    compile_error!("no impls for non-macos targets quite yet");
-}
-
-struct AsyncTcpListener {
-    listener: TcpListener,
-}
-
-impl AsyncTcpListener {
-    pub fn bind(addr: &str) -> io::Result<Self> {
-        let listener = TcpListener::bind(addr)?;
-        listener.set_nonblocking(true)?;
-        Ok(Self { listener })
-    }
-}
-
-impl AsRawFd for AsyncTcpListener {
-    fn as_raw_fd(&self) -> RawFd {
-        self.listener.as_raw_fd()
-    }
-}
-
-#[derive(Debug)]
-pub enum Waitable {
-    /// Registered file descriptor.
-    Fd(RawFd, Interest),
-    // See: https://github.com/smol-rs/polling/blob/1ee697a750f0b3a3b009294130eed13aa7bc994d/src/kqueue.rs
-    // /// Signal.
-    // Signal(std::os::raw::c_int, Interest),
-    // /// Process ID.
-    // Pid(rustix::process::Pid, Interest),
-    // /// Timer.
-    // Timer(usize, Interest),
-}
-
-#[derive(Debug)]
-pub enum Interest {
-    Read,
-    Write,
-    ReadWrite,
-}
-
-/// A representation of an asynchronous computation.
-pub trait Future {
-    type Output;
-    fn poll(&mut self, ready: &[Waitable]) -> impl Iterator<Item = Waitable>;
-    fn take(&mut self) -> Option<Self::Output>;
-}
-
-/// A conversion into an asynchronous computation.
-pub trait IntoFuture {
-    type Output;
-    type IntoFuture: Future<Output = Self::Output>;
-    fn into_future(self) -> Self::IntoFuture;
-}
-
-impl<T> IntoFuture for T
-where
-    T: Future,
-{
-    type Output = T::Output;
-    type IntoFuture = T;
-    fn into_future(self) -> Self::IntoFuture {
-        self
-    }
-}
+use rustix::event::kqueue;
+use std::io::{self, Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::os::fd::{AsFd, AsRawFd, BorrowedFd, OwnedFd, RawFd};
+use std::thread;
+use std::time::Duration;
 
 struct Poller {
     queue: OwnedFd,
-    event_list: Vec<kqueue::Event>,
+    events: Vec<kqueue::Event>,
 }
 
 impl Poller {
-    pub(crate) fn new() -> io::Result<Self> {
-        let queue = kqueue::kqueue()?;
+    pub fn open() -> io::Result<Self> {
         Ok(Self {
-            queue,
-            event_list: vec![],
+            queue: kqueue::kqueue()?,
+            events: Vec::with_capacity(1),
         })
     }
-    /// Registers interest in events descripted by `Event` in the given file descriptor referrred to
-    /// by `file`.
-    pub(crate) fn register(&mut self, event: Waitable) -> io::Result<()> {
-        match dbg!(event) {
-            Waitable::Fd(fd, interest) => {
-                let flags = EventFlags::ADD | EventFlags::RECEIPT | EventFlags::CLEAR;
-                match interest {
-                    Interest::Read => {
-                        dbg!("read");
-                        let event = kqueue::Event::new(EventFilter::Read(fd), flags, 1234);
-                        unsafe {
-                            kqueue::kevent(&self.queue, &[event], &mut self.event_list, None)?
-                        };
-                    }
-                    Interest::Write => {
-                        dbg!("write");
-                        let event = kqueue::Event::new(EventFilter::Write(fd), flags, 1);
-                        unsafe {
-                            kqueue::kevent(&self.queue, &[event], &mut self.event_list, None)?
-                        };
-                    }
-                    Interest::ReadWrite => {
-                        dbg!("read-write");
-                        let events = [
-                            kqueue::Event::new(EventFilter::Read(fd), flags, 0),
-                            kqueue::Event::new(EventFilter::Write(fd), flags, 0),
-                        ];
-                        unsafe {
-                            kqueue::kevent(&self.queue, &events, &mut self.event_list, None)?
-                        };
-                    }
-                };
 
-                // Check for errors
-                let timer = None;
-                unsafe { kqueue::kevent(&self.queue, &[], &mut self.event_list, timer)? };
-                for event in self.event_list.iter() {
-                    check_event_error(event)?;
+    // Register the client for interest in read events, and don't wait for events to come in.
+    //
+    // Safety: we won't polling this after the TcpStream referred to closes, and we delete the
+    // event too.
+    //
+    // Though the rustix docs say that the kqueue must be closed first, this isn't technically true.
+    // You could delete the event as well, and failing to do so isn't actually catastrophic - the
+    // worst case is more spurious wakes.
+    pub fn register_read(&mut self, fd: RawFd) -> io::Result<usize> {
+        // add a notification for read-readiness
+        let notif = kqueue::EventFilter::Read(fd);
+        // add a level-triggered event
+        //
+        // add EV_CLEAR too to make it edge-triggered, which is generally what you want in
+        // practice, but that is a discussion for another time
+        let flags = kqueue::EventFlags::ADD;
+        // 7 seems like a nice number to assert we get back!
+        let udata = 7;
+        let event = kqueue::Event::new(notif, flags, udata);
+
+        // pass in no timeout, and wait indefinitely for events
+        let timeout = None;
+        let event_count =
+            unsafe { kqueue::kevent(&self.queue, &[event], &mut self.events, timeout)? };
+        Ok(event_count)
+    }
+
+    fn wait(&mut self) -> io::Result<usize> {
+        // safety: we are not modifying the list, just polling
+        let event_count =
+            unsafe { kqueue::kevent(self.queue.as_fd(), &[], &mut self.events, None)? };
+        Ok(event_count)
+    }
+
+    pub fn unregister_read(&mut self, fd: RawFd) -> io::Result<usize> {
+        let change_list = &[kqueue::Event::new(
+            kqueue::EventFilter::Read(fd),
+            // remove the event
+            kqueue::EventFlags::DELETE,
+            7,
+        )];
+
+        // we are not waiting on events this time, no need to pass in a real buffer
+        let mut event_list = Vec::new();
+
+        // dont block
+        let timeout = Some(Duration::ZERO);
+        let event_count =
+            unsafe { kqueue::kevent(&self.queue, change_list, &mut event_list, timeout)? };
+        Ok(event_count)
+    }
+}
+
+impl AsFd for Poller {
+    fn as_fd(&self) -> BorrowedFd<'_> {
+        self.queue.as_fd()
+    }
+}
+
+fn main() -> io::Result<()> {
+    // kickoff a simple echo server we can hit for demo purposes
+    thread::spawn(run_echo_server);
+
+    // create the kqueue instance
+    let mut poller = Poller::open()?;
+
+    // set up the client
+    let mut client = TcpStream::connect("127.0.0.1:8080")?;
+    client.set_nonblocking(true)?;
+
+    // we have not written anything yet, this should get EWOULDBLOCK
+    assert_eq!(
+        std::io::ErrorKind::WouldBlock,
+        client.read(&mut [0; 32]).unwrap_err().kind()
+    );
+
+    // send some data over the wire, and wait 1 second for the server to hopefully echo it back
+    client.write(b"hello, world!").unwrap();
+    thread::sleep(Duration::from_secs(1));
+
+    let mut event_count = poller.register_read(client.as_raw_fd())?;
+
+    // we loop due to spurious events being a possibility, polling may need to be retried
+    loop {
+        if event_count == 1 {
+            // verify that the event has the user data we specified, this is just to show udata in
+            // action
+            assert_eq!(7, poller.events[0].udata());
+
+            let mut buffer = [0; 32];
+
+            match client.read(&mut buffer) {
+                Ok(n) => {
+                    assert_eq!(b"hello, world!", &buffer[..n]);
+                    break;
                 }
-                self.event_list.clear();
-
-                Ok(())
+                Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                    event_count = poller.wait()?
+                }
+                Err(e) => {
+                    panic!("Unexpected error {e:?}");
+                }
             }
         }
     }
 
-    pub(crate) fn wait(&mut self) -> io::Result<&[kqueue::Event]> {
-        assert_eq!(self.event_list.len(), 0, "doesn't hold any output");
+    // cleanup by removing the event watch
+    poller.unregister_read(client.as_raw_fd())?;
 
-        // wait for all events
-        let timer = None;
-        unsafe { kqueue::kevent(&self.queue, &[], &mut self.event_list, timer)? };
+    thread::sleep(Duration::from_secs(1));
+    println!("done");
+    Ok(())
+}
 
-        // Handle any possible errors
-        for event in &self.event_list {
-            check_event_error(event)?;
-        }
+fn run_echo_server() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
-        Ok(&self.event_list)
+    loop {
+        let conn = listener.accept().unwrap();
+
+        thread::spawn(move || handle_new_echo_server_connection(conn.0));
     }
 }
 
-/// Check the returned kevent error.
-fn check_event_error(event: &Event) -> io::Result<()> {
-    let data = event.data();
-    if event.flags().contains(kqueue::EventFlags::ERROR)
-            && data != 0
-            && data != Errno::NOENT.raw_os_error() as _
-            // macOS can sometimes throw EPIPE when registering a file descriptor for a pipe
-            // when the other end is already closed, but kevent will still report events on
-            // it so there's no point in propagating it...it's valid to register interest in
-            // the file descriptor even if the other end of the pipe that it's connected to
-            // is closed
-            // See: https://github.com/tokio-rs/mio/issues/582
-            && data != Errno::PIPE.raw_os_error() as _
-    {
-        return Err(io::Error::from_raw_os_error(data as _));
-    } else {
-        Ok(())
-    }
+fn handle_new_echo_server_connection(conn: TcpStream) {
+    println!("connection received, waiting 1 sec");
+    thread::sleep(Duration::from_secs(1));
+    println!("wait over");
+    std::io::copy(&mut &conn, &mut &conn).unwrap();
 }
